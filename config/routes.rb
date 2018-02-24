2a8bcd5d862625442060d9a1b581c0bdf35bffa7
RGu0000 <ryszardgurzeda@gmail.com>
2018-02-24 21:29:20 +0100

    Create Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'leagues#index'
+  resources :players
+  resources :leagues
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
