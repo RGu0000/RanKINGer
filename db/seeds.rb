3bdad76563193c17ffbca3d725ef9f59075b9d9a
RGu0000 <ryszardgurzeda@gmail.com>
2018-02-25 20:07:55 +0100

    Clean connections between tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+=begin
 players = Player.create([
                             {name: 'Mario', email: 'mario@bros.com'},
                             {name: 'Luigi', email: 'luigi@bros.com'},
                             {name: 'Peter', email: 'peter@gmail.com'},
                             {name: 'Han', email: 'han@solo.sw'}
                          ])
+
 leagues = League.create([
                             {name: 'Warsaw league', description: 'Players from Warsaw office - join us!'},
                             {name: 'Poznan league', description: 'Players from Poznan office - join us!'},
                             {name: 'Poland - best 16', description: 'Best 16 from all polish offices'},
                             {name: 'Playground', description: 'Want to test your new tactics? Do it here!'}
                         ])
+=end
+
+players = Player.all
+leagues = League.all
 memberships = Membership.create([
     {league_id: leagues[0].id, player_id: players[0].id},
     {league_id: leagues[0].id, player_id: players[1].id},
