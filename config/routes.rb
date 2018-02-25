aaf19e3cbfc03baae10b25b30c7b9484a7b61e3b
RGu0000 <ryszardgurzeda@gmail.com>
2018-02-25 19:27:05 +0100

    Update routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :players
-  resources :players
-  resources :leagues
-  resources :memberships
-  resources :matches
+  resources :players, only: [:show, :index, :edit, :update, :delete]
+  resources :leagues, only: [:show, :index, :edit, :update, :delete]
+  resources :memberships, only: [:show, :index, :edit, :update, :delete]
+  resources :matches, only: [:show, :index, :edit, :update, :delete]
   root 'leagues#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
