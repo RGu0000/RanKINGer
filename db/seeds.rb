7d3340aaf9dbec042449169666e1e60e71ecb103
RGu0000 <ryszardgurzeda@gmail.com>
2018-02-25 11:19:05 +0100

    Create seeds for a DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+players = Player.create([
+                            {name: 'Mario', email: 'mario@bros.com'},
+                            {name: 'Luigi', email: 'luigi@bros.com'},
+                            {name: 'Peter', email: 'peter@gmail.com'},
+                            {name: 'Han', email: 'han@solo.sw'}
+                         ])
+leagues = League.create([
+                            {name: 'Warsaw league', description: 'Players from Warsaw office - join us!'},
+                            {name: 'Poznan league', description: 'Players from Poznan office - join us!'},
+                            {name: 'Poland - best 16', description: 'Best 16 from all polish offices'},
+                            {name: 'Playground', description: 'Want to test your new tactics? Do it here!'}
+                        ])
+
+
+
