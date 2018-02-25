52072fda6128f6a6ba733c468e92e66aaae913fb
RGu0000 <ryszardgurzeda@gmail.com>
2018-02-25 11:57:12 +0100

    Create new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,22 @@ leagues = League.create([
                             {name: 'Poland - best 16', description: 'Best 16 from all polish offices'},
                             {name: 'Playground', description: 'Want to test your new tactics? Do it here!'}
                         ])
+memberships = Membership.create([
+    {league_id: leagues[0].id, player_id: players[0].id},
+    {league_id: leagues[0].id, player_id: players[1].id},
+    
+    {league_id: leagues[1].id, player_id: players[2].id},
+    {league_id: leagues[1].id, player_id: players[3].id},
+
+    {league_id: leagues[2].id, player_id: players[1].id},
+    {league_id: leagues[2].id, player_id: players[3].id},
+
+    {league_id: leagues[3].id, player_id: players[0].id},
+    {league_id: leagues[3].id, player_id: players[1].id},
+    {league_id: leagues[3].id, player_id: players[2].id},
+    {league_id: leagues[3].id, player_id: players[3].id},
+                                ])
+
 
 
 
